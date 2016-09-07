5b767dfdd54ddf378e70ed0035867ba2914baba7
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-09-07 12:30:46 +0200

    basic ios version

--- index.ios.js
+++ index.ios.js
@@ -9,24 +9,47 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  AlertIOS,
+  TouchableHighlight,
+  DrawerLayoutAndroid
 } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+
+        <View>
+              <Image
+                    style={{width: 150, height: 150}}
+                    borderWidth={2}
+                    marginTop={70}
+                    marginLeft={10}
+                    source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+                      />
+
+              <View style={styles.container}>
+                <Text style={styles.welcome}>
+                  Welcome to React Native!
+                </Text>
+
+                <Text
+                    style={{borderWidth: 1, borderColor: '#000000'}}
+                    onPress={() =>
+                      AlertIOS.alert('This is a toast', 'This is a toast')}>
+                    Click me
+                </Text>
+
+                <Text
+                    onPress={() =>
+                        AlertIOS.alert('This is a toast', 'This is a toast')}>
+                    Click me too.
+                </Text>
+              </View>
+
+          </View>
     );
   }
 }
