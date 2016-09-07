928c63c24e1ffa06526251b394b5f4454941d0f8
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-09-07 11:19:47 +0200

    revert DrawerLayout

--- index.android.js
+++ index.android.js
@@ -9,24 +9,48 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  DrawerLayoutAndroid
 } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
+
+    var navigationView = (
+      <View style={styles.sideMenu}>
+                 <Image style={{width: 40, height: 40}} source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />
+                 <Text style={{margin: 10, fontSize: 18, textAlign: 'left'}}
+                           onPress={() =>   this.refs.drawer.closeDrawer() }>First option</Text>
+                 <Text style={{margin: 10, fontSize: 18, textAlign: 'left'}}
+                            onPress={() =>   this.refs.drawer.closeDrawer() }>Second option</Text>
+                 <Text style={{margin: 10, fontSize: 18, textAlign: 'left'}}
+                            onPress={() =>   this.refs.drawer.closeDrawer() }>Third option</Text>
+        </View>
+    );
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+
+      <DrawerLayoutAndroid
+                            ref="drawer"
+                            drawerWidth={230}
+                            drawerPosition={DrawerLayoutAndroid.positions.Left}
+                            renderNavigationView={() => navigationView}>
+
+          <View style={styles.container}>
+            <Text style={styles.welcome}>
+              Welcome to React Native!
+            </Text>
+            <Text style={styles.instructions}>
+              To get started, edit index.android.js
+            </Text>
+            <Text style={styles.instructions}>
+              Double tap R on your keyboard to reload,{'\n'}
+              Shake or press menu button for dev menu
+            </Text>
+          </View>
+
+      </DrawerLayoutAndroid>
     );
   }
 }
